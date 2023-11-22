f120565579e8624c37021a59c2319278ff4add9f
wellingtonccode <wellingtoncmp@hotmail.com>
2023-11-22 11:37:19 -0300

    Production config

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,8 @@ import { env } from "./env"
 
 app
   .listen({
-    port: env.PORT
+    port: env.PORT,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server running!')
